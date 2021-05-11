d23d4bb2bdcfb19e71fa97f0c1baf649ad73f322
Veljko28 <charlibear28@gmail.com>
2021-05-11 15:04:39 +0200

    Adding footer

---
+++ src/frontpage/Footer.js
@@ -0,0 +1,10 @@
+import '../css/main.css';
+
+export default function Footer() {
+
+    return (
+        <footer className="footer">
+            &copy; Copyright 2021 - Powered by Artisoft
+        </footer>
+    )
+}
