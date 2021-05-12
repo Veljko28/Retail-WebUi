3da672db86863f452cfc84b680d5abef7d9fd5e8
Veljko28 <charlibear28@gmail.com>
2021-05-12 18:40:46 +0200

    Sales added

--- src/components/Sales.js
+++ src/components/Sales.js
@@ -45,6 +45,43 @@ export default function Sales() {
             Total: '$360'
         },
 
+        {
+            Id: "#63541",
+            CashierId: "3",
+            SaleDate: "10/05/2021",
+            SubTotal: '$675',
+            Tax: '$75',
+            Total: '$600'
+        },
+
+
+        {
+            Id: "#78912",
+            CashierId: "5",
+            SaleDate: "10/05/2021",
+            SubTotal: '$250',
+            Tax: '$15',
+            Total: '$235'
+        },
+
+
+        {
+            Id: "#12698",
+            CashierId: "1",
+            SaleDate: "11/05/2021",
+            SubTotal: '$755',
+            Tax: '$85',
+            Total: '$670'
+        },
+
+        {
+            Id: "#29453",
+            CashierId: "2",
+            SaleDate: "11/05/2021",
+            SubTotal: '$920',
+            Tax: '$130',
+            Total: '$790'
+        },
     ]);
 
     return (
@@ -54,29 +91,33 @@ export default function Sales() {
         <Grid item xs={12} md={10} >
             <Navbar loggedIn={true} />
             <Typography  
-            style={{marginTop: '5px', display: 'flex', justifyContent: 'center'}} 
+            style={{marginTop: '5px',marginBottom: '15px', display: 'flex', justifyContent: 'center'}} 
             variant="h2" className="blue">Sales</Typography>
             <table className="table">
-                <tr>
-                    <td>Id</td>
-                    <td>Cashier Id</td>
-                    <td>Sale Date</td>
-                    <td>Sub Total</td>
-                    <td>Tax</td>
-                    <td>Total</td>
-                </tr>
-                {sales.map(x => {
-                    return (
-                        <tr>
-                            <td>{x.Id}</td>
-                            <td>{x.CashierId}</td>
-                            <td>{x.SaleDate}</td>
-                            <td>{x.SubTotal}</td>
-                            <td>{x.Tax}</td>
-                            <td>{x.Total}</td>
-                        </tr>
-                    )
-                })}
+                <thead>
+                    <tr style={{backgroundColor: "#0d7dd4", color: "#fff", textAlign: 'center'}}>
+                        <td>Id</td>
+                        <td>Cashier Id</td>
+                        <td>Sale Date</td>
+                        <td>Sub Total</td>
+                        <td>Tax</td>
+                        <td>Total</td>
+                    </tr>
+                </thead>
+                <tbody>
+                    {sales.map(x => {
+                        return (
+                            <tr>
+                                <td>{x.Id}</td>
+                                <td>{x.CashierId}</td>
+                                <td>{x.SaleDate}</td>
+                                <td>{x.SubTotal}</td>
+                                <td>{x.Tax}</td>
+                                <td>{x.Total}</td>
+                            </tr>
+                        )
+                    })}
+                </tbody>
 
             </table>
             <Footer />
