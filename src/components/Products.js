c4ba903a84fe5a67bf4109fbe5503fd93835c48d
Veljko28 <charlibear28@gmail.com>
2021-05-22 22:12:39 +0200

    Additional header settings

--- src/components/Products.js
+++ src/components/Products.js
@@ -31,10 +31,13 @@ const [products,handleProducts] = React.useState([
             <Grid item xs={1}>
                 <SideBar/>
             </Grid>
-            <Grid item xs={10}> 
+            <Grid item xs={10}>
+                <div style={{display: 'flex', justifyContent: 'space-around', borderBottom: '2px solid #888'}}>
                 <Typography variant="h3" className="blue">Retail</Typography>
                 <Typography variant="h4" style={{color: "#888"}}>Products 
-                <span style={{fontSize:"20px", marginBottom: "20px" ,marginLeft: '10px'}}>{products.length}</span></Typography>
+                <span style={{fontSize:"20px",marginLeft: '10px'}}>{products.length}</span></Typography>
+                <button>Add Product</button>
+                </div>
             </Grid>
 
             <Grid item xs={1}/>
