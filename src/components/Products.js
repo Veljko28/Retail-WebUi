497bc540a38b5e5cefa09e858e42fe0f73dc3bbe
Veljko28 <charlibear28@gmail.com>
2021-05-22 22:08:13 +0200

    Header for the product page

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,9 +1,30 @@
 import React from 'react';
-import { Grid } from '@material-ui/core';
+import { Grid, Typography } from '@material-ui/core';
 import SideBar from '../frontpage/SideBar';
 
 export default function Products() {
-
+const [products,handleProducts] = React.useState([
+        {
+            name: 'Polo Shirt',
+            price: '$25',
+            quantity: 171 
+        },
+        {
+            name: 'Black Jeans',
+            price: '$35',
+            quantity: 72 
+        },
+        {
+            name: 'Thin Jacket',
+            price: '$50', 
+            quantity: 218 
+        },
+        {
+            name: 'Hoodie',
+            price: '$45',
+            quantity: 123 
+        },
+    ]);
 
     return (
         <Grid container>
@@ -11,7 +32,9 @@ export default function Products() {
                 <SideBar/>
             </Grid>
             <Grid item xs={10}> 
-                  Hello  
+                <Typography variant="h3" className="blue">Retail</Typography>
+                <Typography variant="h4" style={{color: "#888"}}>Products 
+                <span style={{fontSize:"20px", marginBottom: "20px" ,marginLeft: '10px'}}>{products.length}</span></Typography>
             </Grid>
 
             <Grid item xs={1}/>
