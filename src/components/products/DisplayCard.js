1d335e7fbc9bbaa03758cd0c051a23f0fce47fd5
Veljko28 <charlibear28@gmail.com>
2021-05-22 22:17:24 +0200

    Creating the product card

---
+++ src/components/products/DisplayCard.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Grid} from '@material-ui/core';
+
+
+export default function DisplayCard() {
+
+    return (
+        <Grid container>
+
+        </Grid>
+    );
+}
